cd0704a8b1373cd12c69334271ca9b9f4b52eac5
ram ji <ramjigupta2707@gmail.com>
2025-08-15 18:13:47 +0530

    creating multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@ const express = require("express"); //importing express
 
 const app = express(); //the server has been made.
 
-app.get("/user/:userid/:password/:place", (req, res) => {
-  console.log(req.params); //dynamic route
-  res.send({ firstname: "hello", secondname: "kaise ho sb" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1st response");
+    // res.send("response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd response");
+    // res.send("this is 2 response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3rd response");
+    // res.send("this is 3 response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4nd response");
+    res.send("this is 4 response");
+  }
+);
 
 app.listen(3000, () => {
   console.log("the server is sucessfully listening.");
