2d86b0c3578ea62e355318ee4acfc1835fd1e38a
ram ji <ramjigupta2707@gmail.com>
2025-08-16 19:06:40 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,18 +2,24 @@ const express = require("express"); //importing express
 
 const app = express(); //the server has been made.
 
-const {adminauth,userauth} = require("./middlewares/auth.js");
-app.use("/admin", adminauth);
-app.get("/user",userauth, (req, res) => {  //if token in userauth does not match then it will send unauthorized request and if it matches it will send to next(),user send data response
-  res.send("user data send");
+
+app.get("/getuserdata", (req, res) => {
+  try {
+    //logic of logic call and get user data
+
+    throw new Error("gswduiu");
+    res.send("user data sent");
+  } catch (err) {
+    res.status(500).send("contact your support team");
+  }
 });
 
-app.get("/admin/getalldata", (req, res) => {
-  res.send("all data sent");
+app.use("/", (err, req, res, next) => {
+  // if(err){
+  res.status(500).send("something went wrong");  // it catches the error ,it is error handler.
+  // }
 });
-app.get("/admin/deleteuser", (req, res) => {
-  res.send("Deleted data");
-});
+
 
 app.listen(3000, () => {
   console.log("the server is sucessfully listening.");
