a02ac92780c5f1ddcf8f5893213085813456cd5f
ram ji <ramjigupta2707@gmail.com>
2025-08-19 01:14:49 +0530

    deleting and updating user API

--- src/app.js
+++ src/app.js
@@ -34,6 +34,33 @@ app.get("/user", async (req, res) => {
   }
 });
 
+//delete the user from the database.
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.UserId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("deleted user successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//update the user in database.
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    console.log(user);
+    res.send("user updated sucessfully");
+  } catch (err) {
+    res.send("something went wrong");
+  }
+});
+
 //feed API  -get/feed-get all the data of users for feed.
 
 app.get("/feed", async (req, res) => {
